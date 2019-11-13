34a3021dbd876a6dd5453e0e8f5ba23541c7fb3d
Pawel Lorek <Pawel.Lorek@microscopeit.com>
2019-11-13 11:30:25 +0100

    Updated scripts/plot3dpoints.py

--- scripts/plot3dpoints.py
+++ scripts/plot3dpoints.py
@@ -1,6 +1,8 @@
 from sklearn import svm
 import pickle
 import argparse
+import os
+
 
 #
  
@@ -8,18 +10,7 @@ import argparse
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
-import numpy.random as rnd
-
-
-fig = plt.figure()
-#to bedzie 3d
-ax = fig.add_subplot(111, projection='3d')
-
-#ile punktow
-n = 50
- 
- 
- 
+  
  
 
 
@@ -45,28 +36,56 @@ points_test= pickle.load(infile)
 infile.close()
 
 
+if not os.path.isfile(input_dir +'/classes_names.pkl' ):
+    print("The following file is missing:  "+input_dir+"classes_names.pkl" +" !!!")
+    quit()
+
+infile_classes = open( input_dir +'/classes_names.pkl' , 'rb')
+classes_names = pickle.load(infile_classes)
+infile_classes.close()
+
 X_train,Y_train = points_train[:,1:],points_train[:,0]
-X_test,Y_test = points_train[:,1:],points_train[:,0]
-
-cl0=np.argwhere(Y_train==0)
-cl1=np.argwhere(Y_train==1)
-cl2=np.argwhere(Y_train==2)
-
-ax.scatter(X_train[cl0][:,0][:,0], X_train[cl0][:,1][:,1],X_train[cl0][:,2][:,2],c='r')
-ax.scatter(X_train[cl1][:,0][:,0], X_train[cl1][:,1][:,1],X_train[cl1][:,2][:,2],c='g')
-ax.scatter(X_train[cl2][:,0][:,0], X_train[cl2][:,1][:,1],X_train[cl2][:,2][:,2],c='b')
+X_test,Y_test = points_test[:,1:],points_test[:,0]
 
 
-#ax.scatter(xs, ys, zs, c=c, marker=m)
- 
- 
+if X_train.shape[1]!=3:
+    print("These are not 3d points !!! (they are "+str(X_train.shape[1])+"-dimensional)")
+    quit()
 
-# ~ for c, m  in [('r', 'o' ), ('b', '^' )]:
-    # ~ xs = rnd.rand(n)*20-15
-    # ~ ys = rnd.rand(n)*30-20
-    # ~ zs = rnd.rand(n)*40-20
+#osobne rysunki dla train i test
+
+#TRAIN
+fig_train = plt.figure(1)
+ax_train = fig_train.add_subplot(111, projection='3d')
+
+ax_train.set_title(input_dir +" - TRAIN")	
+
+   
+
+
+for cl in range(0,int(Y_train.max())+1):
+    points = X_train[Y_train==cl];
+    ax_train.scatter(points[:,0], points[:,1], points[:,2], label=classes_names[cl])
     
-plt.title('')
+ax_train.legend()
+ 	   
+    
+# TEST   (na razie nie dziala) 
+    
+# ~ fig_test = plt.figure(2)
+# ~ ax_test = fig_test.add_subplot(111, projection='3d')
+
+# ~ ax_test.set_title(input_dir +" - TRAIN")	
+
+# ~ for cl in range(0,int(Y_test.max())+1):
+    # ~ points = X_test[Y_test==cl];
+    # ~ ax_test.scatter(points[:,0], points[:,1], points[:,2], label=classes_names[cl])
+    
+# ~ ax_test.legend()
+ 	   
+    
+ 
+ 
 plt.show()
 
 quit()
