4bcbcc63c2d3b7f2abff6dac5230932891c03a28
magdamzr <magda0mzr@gmail.com>
2020-01-13 16:51:23 +0100

    Decision Tree classifier

---
+++ scripts/decisiontree.py
@@ -0,0 +1,33 @@
+
+import pickle
+import argparse
+import numpy as np
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics
+
+def ParseArguments():
+    parser = argparse.ArgumentParser(description="Project")
+    parser.add_argument('--input-dir', default="", required=True, help='data dir')
+    parser.add_argument('--output-dir', default="", required=False, help='output dir')
+
+    args = parser.parse_args()
+
+    return (args.input_dir, args.output_dir)
+    
+input_dir, output_dir= ParseArguments()
+
+train = open(input_dir + "train_data.pkl", "rb")
+pointsTrain = pickle.load(train)
+train.close()
+
+test = open(input_dir + "test_data.pkl", "rb")
+pointsTest = pickle.load(test)
+test.close()
+
+XTrain, YTrain = pointsTrain[::,1:],pointsTrain[::,0]
+XTest, YTest = pointsTest[::,1:],pointsTest[::,0]
+
+dtClassifier = DecisionTreeClassifier(criterion = "gini")
+dtClassifier.fit(XTrain, YTrain)
+prediction = dtClassifier.predict(XTest)
+print("Accurancy:", metrics.accuracy_score(YTest, prediction))
